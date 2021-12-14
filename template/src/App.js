0ff33b1b52614529d9e09ff3bafeccf2305f58c0
SwapRane12 <swaprr12@outlook.com>
2021-12-14 12:54:14 +0530

    initial commit

---
+++ template/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="min-h-screen justify-center items-center" >
+        <h1>React Tailwindcss Starter Template</h1>
+
+        <div className="mt-8" >
+          <logo className="animate-spin" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
