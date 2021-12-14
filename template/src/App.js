dccbd37d9fbe1b80ea0dd6f004c417cf1210bc3e
SwapRane12 <swaprr12@outlook.com>
2021-12-14 16:47:45 +0530

    update test

--- template/src/App.js
+++ template/src/App.js
@@ -1,15 +1,13 @@
 import logo from './logo.svg'
 
-
 function App() {
   return (
     <div className="App">
-      <div className=" flex flex-col min-h-screen w-screen justify-center items-center" >
+      <div className=" flex flex-col min-h-screen w-screen justify-center items-center">
         <div>
-          <h1 className="text-3xl text-gray-700" >React Tailwindcss Starter</h1>
-          <div className="mt-8 max-w-screen-sm " >
+          <h1 className="text-3xl text-gray-700">React Tailwindcss Starter</h1>
+          <div className="mt-8 max-w-screen-sm ">
             <img className="animate-spin" src={logo} alt="" srcset="" />
-
           </div>
         </div>
       </div>
--- template/src/App.test.js
+++ template/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders react tailwind starter', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/React Tailwindcss Starter/i)
+  expect(linkElement).toBeInTheDocument()
+})
